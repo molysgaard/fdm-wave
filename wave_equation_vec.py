76520c900a5127ee31a3569a1adb324efdc17770
Morten Olsen Lysgaard <morten@lysgaard.no>
2013-02-25 15:07:35 +0100

    added stuff, new shading

--- wave_equation_vec.py
+++ wave_equation_vec.py
@@ -13,16 +13,19 @@ import os,sys
 import math
 
 # Grid size
-N = 100
+N = 400
 
 # Set step size
 h = 0.01
+# wave speed, must be less than one to make sense
+c = 0.005
+dt = 0.001
 
-# Calculate dt
-dt = 0.0001
 k = dt/h
-NumOfTimeSteps = int(3000)
-plotSteps = 10
+NumOfTimeSteps = int(10000)
+plotSteps = 20
+
+print c*dt*h*h
 
 # Create mesh for plotting
 X, Y = np.mgrid[:N,:N]
@@ -40,6 +43,7 @@ ldiag = np.ones(N*N)
 I = sparse.identity(N*N)
 
 method = [(0,0,-4),(1,0,1),(-1,0,1),(0,1,1),(0,-1,1)]
+#method = [(0,0,-4),(1,0,1),(-1,0,1),(0,1,1),(0,-1,1),(1,1,0),(1,-1,0),(-1,1,0),(-1,-1,0)]
 
 def two_to_one(rows,cols,i,j):
     return i*cols+j
@@ -69,34 +73,36 @@ for i in xrange(N*N):
                 A[i,j] = c
 
 A = sparse.dia_matrix(A)
-comp = (2*I +k*A)
+comp = (2*I +k*c**2*A)
 mask = mask.reshape(N*N,1)
 
 # Init U vectors
-#U_2 = np.zeros((N,N))
 U_1 = np.zeros((N,N))
 
 # Initial wave
 def gauss(x,y):
-    return 1/(2*math.pi)*math.exp(-(x^2 + y^2)/2)
+    return 1/(2*math.pi)*math.exp(-(x**2 + y**2)/2)
 
+# the origin of the initial wave
 x_0 = N/2
 y_0 = N/2
 
-for i in xrange(-5,5):
-    for j in xrange(-5,5):
-        U_1[x_0+j,y_0+i]=gauss(j/5,i/5)
+for i in xrange(-20,20):
+    for j in xrange(-20,20):
+        U_1[x_0+j,y_0+i]=gauss(j/5.0,i/5.0)
 
 U_1 = U_1.reshape(N*N,1)
 U_2 = U_1
+
+colors = [[(0, 0.9, 1, 1.) for x in range(N)] for y in range(N)]
 
 ## Run forloops to calculate
 i = 0
 for n in range(1,NumOfTimeSteps+1):
 
     # Calculate the new vector
-    U_new = (2*I + k*A)*U_1 - U_2
-    #U_new = comp*U_1 - U_2
+    #U_new = (2*I + k*A)*U_1 - U_2
+    U_new = comp*U_1 - U_2
     U_new = np.multiply(U_new,mask)
 
     if n%plotSteps==0:
@@ -105,18 +111,19 @@ for n in range(1,NumOfTimeSteps+1):
         fname = '/tmp/1234/tmp_%05d.png' %i
         i+=1
 
-        plt.imshow(U_new.reshape(N,N))
-        plt.savefig(fname, dpi=100)
-        plt.clf()
-
-        #fig = plt.figure()
-        #ax = fig.add_subplot(1,1,1, projection='3d')
-        #surf = ax.plot_surface(X,Y,np.array(U_new.reshape(N,N))), rstride=5, cstride=5, cmap=cm.ocean)
-        #ax.view_init(elev=30, azim=n/2)
-        #ax.set_zlim3d(-0.06,0.06)
+        #plt.imshow(U_new.reshape(N,N))
         #plt.savefig(fname, dpi=100)
         #plt.clf()
-        #plt.close()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1, projection='3d')
+        surf = ax.plot_surface(X,Y,np.array(U_new.reshape(N,N)), linewidth=0, shade=True, antialiased=False, facecolors=colors)
+        #surf = ax.plot_surface(X,Y,np.array(U_new.reshape(N,N)), rstride=5, cstride=5, cmap=cm.ocean)
+        #ax.view_init(elev=30, azim=n/2)
+        ax.set_zlim3d(-0.1,0.1)
+        plt.savefig(fname, dpi=150)
+        plt.clf()
+        plt.close()
         print "Pic %s of %s created" % (n,NumOfTimeSteps)
 
     U_2 = U_1
